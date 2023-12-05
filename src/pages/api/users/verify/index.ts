d04114818d131855668748bb9354ab5a26237c52
abcei2 <santiago.salgado.duque@gmail.com>
2023-12-04 21:50:31 -0500

    Fix userAuth data assignment in profile.ts and handle error in verifyEmail

--- src/pages/api/users/verify/index.ts
+++ src/pages/api/users/verify/index.ts
@@ -25,21 +25,21 @@ async function verifyEmail(req: NextApiRequest, res: NextApiResponse) {
     const userAuth = getAuthCookie(req);
     if (backResponse.status == 200) {
       const data = await backResponse.json();
-      if (userAuth) {
-        setAuthCookie(res, { ...userAuth, ...data });
-      }
+      setAuthCookie(res, { ...userAuth, ...data });
+      res.writeHead(302, { Location: "/search" });
+      res.end();
+      return;
     } else {
       console.log("Error verifying email");
     }
     // Redirect the user to the homepage
-    res.writeHead(302, { Location: userAuth ? "/search" : "/login" });
-    res.end();
   } catch (err) {
     console.log("invalid token");
     // The token was invalid, return an error
-    res.writeHead(302, { Location: "/" });
-    res.end();
   }
+
+  res.writeHead(401, { Location: "/" });
+  res.end();
 }
 
 export default verifyEmail;
