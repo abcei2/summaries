2976c5da5ab913fa832e9919b13fda427972557a
Santiago Salgado Duque <santiago.s@somosgentedigital.com>
2024-06-23 09:36:57 -0500

    just had to repeat elements to animation works

--- src/components/wrapper/TopNews.tsx
+++ src/components/wrapper/TopNews.tsx
@@ -1,8 +1,36 @@
 const TEXT_ARRAY = [
-  "dense articles*",
-  "case studies  *",
-  "books         *",
-  "youtube videos*",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
+  "dense articles",
+  "case studies",
+  "books",
+  "youtube videos",
 ];
 
 const TopNews = ({
@@ -10,17 +38,22 @@ const TopNews = ({
 }: {
   backgroundColor?: "bg-primary" | "bg-secondary" | "bg-tertiary";
 }) => {
-  const amountOfRepeats = 2;
   return (
-    <div className={`w-full  flex ${backgroundColor}`}>
-      <div className="w-full flex gap-5 animate-ltrTranslation25">
+    <div className={`w-full relative flex overflow-hidden ${backgroundColor}`}>
+      <div className="animate-rtlTranslation25 whitespace-nowrap flex gap-5">
         {TEXT_ARRAY.map((text, index) => (
-          <div
-            key={index}
-            className="flex items-center text-3xl justify-center"
-          >
+          <div className="flex items-center text-3xl justify-center">
             <span>{text}</span>
-            <span className="text-3xl flex"> * </span>
+            <span className="text-3xl flex h-fit"> * </span>
+          </div>
+        ))}
+      </div>
+
+      <div className="absolute top-0 animate-ltrTranslation25 whitespace-nowrap flex  gap-5 ml-5">
+        {TEXT_ARRAY.map((text, index) => (
+          <div className="flex items-centertext-3xl">
+            <span className="text-3xl">{text}</span>
+            <span className="text-xl"> * </span>
           </div>
         ))}
       </div>
