6a2c575b7d55786a0135ee8e08873ffd2210769d
Santiago Salgado Duque <santiago.s@somosgentedigital.com>
2024-07-08 21:23:11 -0500

    Hightlight section styled

--- src/components/MyHighlights.tsx
+++ src/components/MyHighlights.tsx
@@ -1,6 +1,7 @@
 // components/MyHighlights.tsx
 import { useEffect, useState } from "react";
 import { HiCog } from "react-icons/hi";
+import CustomImage from "./utils/CustomImage";
 
 type Highlight = {
   id: number;
@@ -20,11 +21,13 @@ type Highlight = {
 
 const HighlightCard = ({ highlight }: { highlight: Highlight }) => {
   const [commentary, setCommentary] = useState<string>(""); // State to hold commentary
+  const [currentHighlightCommentary, setCurrentHighlightCommentary] =
+    useState<string>("");
   const [editing, setEditing] = useState<boolean>(false); // State to manage edit mode
 
   useEffect(() => {
-    
     setCommentary(highlight.comment);
+    setCurrentHighlightCommentary(highlight.comment);
   }, [highlight]);
 
   const handleUpdate = () => {
@@ -41,53 +44,74 @@ const HighlightCard = ({ highlight }: { highlight: Highlight }) => {
           alert("Error updating commentary");
         } else {
           setEditing(false);
+          setCurrentHighlightCommentary(commentary);
         }
       });
-    
   };
 
   return (
-    <div className="w-full rounded-lg shadow-lg border border-2 flex flex-col mb-4">
-      <div className="text-center p-2">
-        <span className="font-bold text-gray-600 mb-2">
+    <div className="w-full rounded-[10px] border border-custom-black flex flex-col gap-3 text-xs py-4 px-6 font-pt-sans">
+      <div className="flex flex-col gap-3 text-center">
+        <span className="font-bold font-rokkitt">
           {highlight.book_name} (Summary ID: {highlight.summary_id})
         </span>
+        <span className="text-[10px]">
+          Created At: {new Date(highlight.created_at).toLocaleString()}
+        </span>
       </div>
-      
-      <div className="flex flex-col p-2">
-        <span className="text-gray-600">{highlight.text}</span>
-        <span>Created At: {new Date(highlight.created_at).toLocaleString()}</span>
-        <span>Commentary: </span>
-        
+
+      <div className="flex flex-col gap-2 h-[100px] overflow-auto">
+        <span className="font-bold">{"Selected text"}</span>
+        <span className="">{highlight.text}</span>
+      </div>
+
+      <div className="flex flex-col gap-2 h-[80px]">
+        <span className="font-bold">Commentary: </span>
         {editing ? (
-          <textarea 
+          <textarea
             value={commentary || ""}
-            onChange={(e) => setCommentary(e.target.value)} 
-            rows={3} 
+            onChange={(e) => setCommentary(e.target.value)}
+            className="rounded-[10px] p-2"
+            rows={3}
             cols={50}
           />
         ) : (
-          commentary
+          <span className="overflow-auto">{commentary}</span>
         )}
+      </div>
+
+      <div className="flex flex-col gap-2">
         {!editing && (
-        <label className="flex items-center bg-primary rounded cursor-pointer w-40 justify-center mt-2">
-        <button className="mr-2 text-sm text-white" onClick={() => setEditing(!editing)}>Edit</button>
-      </label>
-      )}
-      {editing && (
-        <label className="flex items-center bg-primary rounded cursor-pointer w-40 justify-center mt-2">
-          <button className="mr-2 text-sm text-white" onClick={handleUpdate}>Save</button>
-        </label>
-      )}
+          <button
+            className="btn bg-secondary border-secondary text-gray-200 font-extralight w-[118px] self-center"
+            onClick={() => setEditing(!editing)}
+          >
+            Edit
+          </button>
+        )}
+        {editing && (
+          <div className="flex self-center gap-1">
+            <button
+              className="btn bg-tertiary border-tertiary font-extralight w-[118px]"
+              onClick={handleUpdate}
+            >
+              Save
+            </button>
+            <button
+              className="btn bg-custom-red border-custom-red font-extralight w-[118px]"
+              onClick={() => {
+                setEditing(false);
+                setCommentary(currentHighlightCommentary);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
 };
-
-
-
-
-
 
 const MyHighlights = () => {
   const [myHighlights, setMyHighlights] = useState<Highlight[]>();
@@ -105,22 +129,29 @@ const MyHighlights = () => {
   }, []);
 
   return (
-    <div className="w-full flex flex-col items-center">
-      <div className="w-full bg-[#F8F8F8] h-20 flex items-center justify-center">
-        <span className="text-4xl font-bold text-gray-600">
-          My Highlights
-        </span>
+    <div className="w-full flex flex-col items-center sm:items-start gap-10">
+      <div className="flex gap-2 items-center ">
+        <CustomImage
+          src="/icons/asterisc.svg"
+          alt="punctuation"
+          width={18}
+          height={18}
+        />
+        <span className="text-2xl font-bold font-rokkitt">My Highlights</span>
       </div>
-      <div className="w-full h-full flex flex-col items-center p-4">
+      <div className="w-full h-full flex flex-col items-center sm:p-4 ">
         {!loading ? (
-          <div className="w-full flex flex-col items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-8">
             {myHighlights &&
               myHighlights
-                .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+                .sort(
+                  (a, b) =>
+                    new Date(b.created_at).getTime() -
+                    new Date(a.created_at).getTime()
+                )
                 .map((highlight) => (
                   <HighlightCard key={highlight.id} highlight={highlight} />
                 ))}
-
           </div>
         ) : (
           <div className="flex flex-col items-center justify-center text-center text-gray-500 mt-10">
--- src/pages/myhighlights.tsx
+++ src/pages/myhighlights.tsx
@@ -1,9 +1,12 @@
 import MyHighlights from "@/components/MyHighlights";
+import MainContainer from "@/components/utils/MainContainer";
 
 const page = () => {
-  
-  
-  return (<MyHighlights /> );
+  return (
+    <MainContainer className="min-h-screen overflow-auto ">
+      <MyHighlights />
+    </MainContainer>
+  );
 };
 
 export default page;
